8be5374952d2b41eaa3c38735a13bd97ff1cdc6f
Benjamin <BensThoughts@Gmail.Com>
2022-02-14 11:01:43 -0700

    setup server

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -2,15 +2,33 @@ import { renderToString } from "react-dom/server";
 import { RemixServer } from "remix";
 import type { EntryContext } from "remix";
 
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+import createEmotionServer from '@emotion/server/create-instance';
+
+const key = 'custom';
+const cache = createCache({ key });
+const {
+  extractCriticalToChunks,
+  constructStyleTagsFromChunks,
+} = createEmotionServer(cache);
+
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
+  let markup = renderToString(
+    <CacheProvider value={cache}>
+      <RemixServer context={remixContext} url={request.url} />
+    </CacheProvider>
   );
+
+  const chunks = extractCriticalToChunks(markup);
+  const styles = constructStyleTagsFromChunks(chunks);
+
+  markup = markup.replace('__STYLES__', styles);
 
   responseHeaders.set("Content-Type", "text/html");
 
